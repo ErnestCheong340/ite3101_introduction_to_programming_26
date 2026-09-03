d2cd05df5856941126735284576c2e1386c2f2c2
ErnestCheong340 <ernestcheong340@gmail.com>
2026-09-03 08:44:40 +0000

    [skip actions] lab/lab01/ch01_t04_handling_errors.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,5 +1,5 @@
 # print("How do you make a hot dog stand?')
 # print(You take away its chair!)
 
-print("Mismatched quotes will cause a SyntaxError')
+print("Mismatched quotes will cause a SyntaxError")
 print() Without quotes will cause a NameError
