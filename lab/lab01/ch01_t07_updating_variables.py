497f90e92f6f9dd0e2b9810d17cc5294c0e7e18e
ErnestCheong340 <ernestcheong340@gmail.com>
2026-09-03 08:57:27 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -12,4 +12,7 @@ october_rainfall = 7.20
 november_rainfall = 5.06
 december_rainfall = 4.06
 
-annual_rainfall = ()
+annual_rainfall = (september_rainfall = 5.16
+october_rainfall = 7.20
+november_rainfall = 5.06
+december_rainfall)
