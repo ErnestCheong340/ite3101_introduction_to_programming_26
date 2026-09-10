4b62b180cdcc02bf72be80dd2efaf52502eb4e86
ErnestCheong340 <ernestcheong340@gmail.com>
2026-09-10 07:20:56 +0000

    [skip actions] lab/lab01/ch01_t05_variables.py

--- lab/lab01/ch01_t05_variables.py
+++ lab/lab01/ch01_t05_variables.py
@@ -1,2 +1,2 @@
->       self.assertEqual(temp_locals['todays_date'], f"{datetime.datetime.now():%d/%m/%Y}")
+temp_locals['todays_date'], f"{datetime.datetime.now():%d/%m/%Y}")
 todays_date = "03/09/2026",
